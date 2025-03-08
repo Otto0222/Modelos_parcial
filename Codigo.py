811fcea99d167af5eb8728e124cc1d474838176e
Otto0222 <otto0222@gmail.com>
2025-03-07 20:33:38 -0500

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 import streamlit as st
+
 
 #Load dataset
 df = pd.read_csv('university_student_dashboard_data.csv')
