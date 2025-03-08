27c39c90a1f7fc3dbd8c80bca1e901408ad25d65
Otto0222 <otto0222@gmail.com>
2025-03-07 20:04:11 -0500

    Create Codigo.py

---
+++ Codigo.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+#Load dataset
+df = pd.read_csv('university_student_dashboard_data.csv')
+
+#Inspect dataset
+print(df.head())
+
+
+# Sidebar Filters
+st.sidebar.header("Filters")
+selected_term = st.sidebar.selectbox("Select Term", df["Term"].unique())
+selected_department = st.sidebar.multiselect("Select Departments", df["Department"].unique(), default=df["Department"].unique())
+
+# Filtered Data
+df_filtered = df[(df["Term"] == selected_term) & (df["Department"].isin(selected_department))]
+
+# KPI Calculations
+total_applications = df_filtered["Applications"].sum()
+total_admissions = df_filtered["Admissions"].sum()
+total_enrollments = df_filtered["Enrollments"].sum()
+retention_rate = df_filtered["Retention Rate"].mean()
+satisfaction_score = df_filtered["Satisfaction Score"].mean()
