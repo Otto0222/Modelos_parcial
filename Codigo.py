d9597c39b4fc68d16451aafea81b36dbf0cee5a4
Otto0222 <otto0222@gmail.com>
2025-03-07 20:32:06 -0500

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -10,11 +10,17 @@ df = pd.read_csv('university_student_dashboard_data.csv')
 print(df.head())
 
 
+# Interface
 # Sidebar Filters
 st.sidebar.header("Filters")
 selected_year = st.sidebar.selectbox("Select Year", df["Year"].unique())
 selected_term = st.sidebar.selectbox("Select Term", df["Term"].unique())
 
+# Dashboard Title
+st.title("University Admissions & Student Satisfaction Dashboard")
+
+
+# Total applications, admissions and enrollments per term
 # Filter Data
 filtered_df = df[(df["Year"] == selected_year) & (df["Term"] == selected_term)]
 
@@ -24,16 +30,28 @@ if not filtered_df.empty:
     total_applications = filtered_df["Applications"].sum()
     total_admissions = filtered_df["Admitted"].sum()
     total_enrollments = filtered_df["Enrolled"].sum()
-    retention_rate = filtered_df["Retention Rate (%)"].mean()
-    satisfaction_score = filtered_df["Student Satisfaction (%)"].mean()
 
-
-# Dashboard Title
-st.title("University Admissions & Student Satisfaction Dashboard")
-
-# Display KPIs
+# Display Total applications, admissions, and enrollments per term
 st.metric("Total Applications", total_applications)
 st.metric("Total Admissions", total_admissions)
 st.metric("Total Enrollments", total_enrollments)
-st.metric("Average Retention Rate", f"{retention_rate:.2f}%")
-st.metric("Average Satisfaction Score", f"{satisfaction_score:.2f}/10")
+
+
+# Retention rate trends over time
+# Group the retention rates by years
+df_retention_trend = df.groupby(["Year"])["Retention Rate (%)"].mean().reset_index()
+
+# Plot the retention rate trends across the years 
+fig_retention = px.line(df_retention_trend, x="Year", y="Retention Rate (%)", title="Retention Rate Trends")
+st.plotly_chart(fig_retention)
+
+
+# Student Satisfaction Score over the years
+# Group the student satisfaction by years
+df_satisfaction_trend = df.groupby(["Year"])["Student Satisfaction (%)"].mean().reset_index()
+
+# Plot the retention rate trends across the years 
+fig_satisfaction = px.line(df_satisfaction_trend, x="Year", y="Student Satisfaction (%)", title="Student Satisfaction Trends")
+st.plotly_chart(fig_satisfaction)
+
+
